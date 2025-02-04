05a429f587b70bc951f34ee4583f4ea363a2de18
Vihaan <vverma270705@gmail.com>
2025-02-04 19:18:46 +0530

    completed CI/CD pipeline

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello, AWS Elastic Beanstalk with Docker!');
+    res.send('Hello AWS Elastic Beanstalk with Docker!');
 });
 
 app.listen(port, () => {
