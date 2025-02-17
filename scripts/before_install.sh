86c3965f674c5f5999d036455fc23336d22f554d
Vihaan <vverma270705@gmail.com>
2025-02-17 14:32:17 +0530

    cd-1

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -4,6 +4,7 @@
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 . ~/.nvm/nvm.sh
 nvm install node
+npm install -g pm2
 
 #create our working directory if it doesnt exist
 DIR="/home/ubuntu/wati-webhook"
