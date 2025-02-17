f7cdb150c4b0b544ddb0ef054c567b1e1c73408d
Vihaan <vverma270705@gmail.com>
2025-02-17 14:01:36 +0530

    cicd

--- wati-webhook/server.js
+++ wati-webhook/server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("CICD FROM CODE DEPLOY! ci/cd codepipeline");
+  res.send("CICD FROM CODE DEPLOY! ci/cd");
 });
 
 app.listen(port, () => {
