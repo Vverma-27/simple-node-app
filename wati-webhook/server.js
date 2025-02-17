30db8a15f01c8466eeef94d5bf7d48cddb0a370a
Vihaan <vverma270705@gmail.com>
2025-02-17 19:59:02 +0530

    checking

--- wati-webhook/server.js
+++ wati-webhook/server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("CICD FROM CODE ! hi everyone ");
+  res.send("CICD FROM CODE ! hi hahhahahaajjajajajakakaklalal ");
 });
 
 app.listen(port, () => {
