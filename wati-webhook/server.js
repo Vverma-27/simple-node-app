ad2b28f6ad5a176e7c466243a973992f0b135d69
Vihaan <vverma270705@gmail.com>
2025-02-17 14:06:37 +0530

    pulled

--- wati-webhook/server.js
+++ wati-webhook/server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 80;
 
 app.get("/", (req, res) => {
-  res.send("CICD FROM CODE DEPLOY! ci/cd codepipeline");
+  res.send("CICD FROM CODE DEPLOY! ci/cd ");
 });
 
 app.listen(port, () => {
